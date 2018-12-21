9d0859e9272a4979d72da63a2097b4b8d1a037b0
Lwenli <102495553@qq.com>
2018-12-21 17:45:43 +0800

    封装样式方法

---
+++ week3/day4/utils.js
@@ -0,0 +1,105 @@
+window.utilObj = (function() {
+  // 将json字符串格式化json对象
+  function jsonParse(data) {
+    return "JSON" in window ? JSON.parse(data) : eval(`(${data})`);
+  }
+
+  // 类数组转数组
+  function likeAryTo(likeAry) {
+    try {
+      return [].slice.call(likeAry);
+    } catch (e) {
+      var ary = [];
+      for (let i = 0; i < likeAry.length; i++) {
+        ary[i] = likeAry[i];
+      }
+      return ary;
+    }
+  }
+
+  // offset 获取页面元素距离body的偏移位置
+  function offset(ele) {
+    let left = ele.offsetLeft,
+      top = ele.offsetTop,
+      parent = ele.offsetParent;
+    while (parent && parent.nodeName.toUpperCase() !== "BODY") {
+      left += parent.clientLeft + parent.offsetLeft;
+      top += parent.clientTop + parent.offsetTop;
+      parent = parent.offsetParent;
+    }
+    return {
+      left,
+      top
+    };
+  }
+
+  // getCss 获取样式
+  function getCss(ele, attr) {
+    let value
+    if ("getComputedStyle" in window) {
+      value = window.getComputedStyle(ele, null)[attr]
+    } else {
+      if (attr === 'opacity') {
+        // opacity: 0.5
+        // "alpha(opacity=50)"
+        value = ele.currentStyle['filter']
+        let reg = /^alpha\(opacity=(.+)\)$/i
+        // if (reg.test(value)) {
+        //   value = reg.exec(value)[1] / 100
+        // } else {
+        //   value = 1
+        // }
+        value = reg.test(value) ? reg.exec(value)[1] / 100 : 1
+      } else {
+        value = ele.currentStyle[attr]
+      }
+    }
+
+    // 去单位 "12px" "0.5" px pt rem em display: inline-block
+    let reg = /^-?\d+(\.\d+)?(px|pt|rem|em)?$/i
+    if (reg.test(value)) {
+      value = parseFloat(value)
+    }
+    return value
+  }
+
+  // setCss 设置样式
+  function setCss(ele, attr, value) {
+    if (attr === 'opacity') {
+      ele.style['opacity'] = value
+      ele.style['fillter'] = `alpha(opacity=${value * 100})`
+      return
+    }
+    
+    let reg = /^(width|height|((margin|padding)?(left|top|right|bottom)?))$/i
+    
+    if (reg.test(attr)) {
+      if (!isNaN(value)) {
+        value += 'px'
+      }
+    }
+
+    ele.style[attr] = value
+  }
+
+  // 批量设置css
+  function setCssBatch(ele, options) {
+    let isObj = Object.prototype.toString.call(options) === '[object Object]'
+    if (isObj) {
+      for (let attr in options) {
+        if (options.hasOwnProperty(attr)) {
+          setCss(ele, attr, options[attr])
+        }
+      }
+    }
+  }
+
+  return {
+    jsonParse,
+    likeAryTo,
+    offset,
+    getCss,
+    setCss,
+    setCssBatch
+  };
+})();
