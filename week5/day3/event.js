b73680dc2a2def3039d5d6b51b2723dc2a41cf65
renjinhui <1227760869@qq.com>
2019-01-02 18:31:42 +0800

    m

---
+++ week5/day3/event.js
@@ -0,0 +1,34 @@
+function on(ele,type,f) {
+    if(/^my\w+/.test(type)){
+        ele[type] = ele[type] || [];
+        ele[type].push(f);
+    }else{
+        type = type.replace(/on/g,'')
+        ele.addEventListener(type,f,false);
+    }
+    
+}
+function fire(ele,type) {
+    ele[type] = ele[type] || [];
+    for(let i =0; i < ele[type].length;i++){
+        ele[type][i] && ele[type][i].call(ele)
+    }
+    let arr = [];
+    ele[type].map((item)=>{
+        item === null ? null : arr.push(item)
+    })
+    ele[type] = arr;
+}
+
+function off(ele,type,f){
+    if(/^my\w+/.test(type)){
+        ele[type] = ele[type] || [];
+        ele[type] = ele[type].map((item)=>{
+            return item !==f ? item : null
+        });
+    }else{
+        type = type.replace(/on/g,'');
+        ele.removeEventListener(type,f,false);
+    }
+    
+}
