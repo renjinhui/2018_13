bb25c630abe19c4fbcb6037f93dcbb7e9fd88b72
renjinhui <1227760869@qq.com>
2019-01-12 13:03:14 +0800

    m

--- week6/sina/js/index.js
+++ week6/sina/js/index.js
@@ -1,18 +1,19 @@
-! function (e, t) {
-    var n = t.documentElement,
-        d = e.devicePixelRatio || 1;
-
-    function i() {
-        var e = n.clientWidth / 3.75;
-        n.style.fontSize = e + "px"
-    }
-    if (function e() {
-            t.body ? t.body.style.fontSize = "16px" : t.addEventListener("DOMContentLoaded", e)
-        }(), i(), e.addEventListener("resize", i), e.addEventListener("pageshow", function (e) {
-            e.persisted && i()
-        }), 2 <= d) {
-        var o = t.createElement("body"),
-            a = t.createElement("div");
-        a.style.border = ".5px solid transparent", o.appendChild(a), n.appendChild(o), 1 === a.offsetHeight && n.classList.add("hairlines"), n.removeChild(o)
-    }
-}(window, document)
+function banner() {
+    new Swiper('.banner_box',{
+        // autoplay:true , // 是否自动播放 默认false
+        autoplay:{
+            delay:2000, // 两张图片之间切换的等待时间
+            disableOnInteraction:false // 用户操作swiper后 是否停止自动播放
+        },
+        loop: true, // 实现无缝滚动
+        speed:200, // 图片滚动的速度
+        pagination:{
+            el:'.tip_box',
+            type:'fraction',
+            currentClass:'current',
+            totalClass:'total'
+        }
+    })
+    // 第一个参数是盒子元素的类名或者id
+}
+banner();
