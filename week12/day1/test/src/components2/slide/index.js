8ba9b82ca853f5aa4f2e6f0412dc27da7511552f
renjinhui <1227760869@qq.com>
2019-03-05 19:31:20 +0800

    m

--- week12/day1/test/src/components2/slide/index.js
+++ week12/day1/test/src/components2/slide/index.js
@@ -8,12 +8,34 @@ import Dot from './dot'
 class Slider extends React.Component {
     constructor() {
         super();
+        this.state = {
+            ary:[1,2,3],
+            index:0 // 控制显示第几张
+        }
         
+    }
+    turn(n){
+        // n 确定 前进或者后退
+        this.setState({
+            index:this.state.index+n
+        })
+    }
+    move(){
+        this.timer = setInterval(()=>{
+            this.turn(1)
+        },3000)
+    }
+    componentDidMount(){
+        this.move();
     }
     render() {
         return <div className='container'>
             <Arrow/>
-            <Item></Item>
+            <Item 
+                data={this.state.ary} 
+                index={this.state.index}
+                initIndex={(n)=>{this.setState({index:n})}}
+                ></Item>
             <Dot/>
         </div>;
     }
--- week12/day1/test/src/components2/slide/item.js
+++ week12/day1/test/src/components2/slide/item.js
@@ -1,13 +1,53 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 class Item extends React.Component {
-    constructor() {
-        super();
-        
+    constructor(props) {
+        super(props);
+        // console.log(111)
+        this.state = {
+            ary:props.data.concat(props.data[0]),// 在最后补一张
+            len:props.data.length+1 // 填补后的数组的长度
+        }
     }
     render() {
-        return <ul className='listBox'>
+        // let {data} = this.props;
+        // data.push(data[0]);// 在最后补一张
+        let {ary,len} = this.state;
+        let {index,initIndex} = this.props;
+        let sty = {
+            width: len*400+'px'
+        };
+        if(index==len){
+            // 接下来 就要露白了
+            // 要求 闪到第一张；然后再从第一张往后走
+            sty = Object.assign(sty,{
+                left:0
+            })
+            // console.log(initIndex)
+            // initIndex(0) // 改变父组件的index
+            setTimeout(() => {
+                initIndex(1) 
+            }, 10);
+        }else{
+            sty = Object.assign(sty,{
+                left:-index*400+'px',
+                transition:'left 0.3s ease'
+            })
+        }
+        // let sty = {
+        //     left:-index*400+'px',
+        //     width:len*400 +'px',
+        //     transition:'left 0.3s ease'
+        // };
 
+        return <ul className='listBox' style={sty}>
+            {
+                ary.map(item=>{
+                    return(
+                        <li key={Math.random()}>{item}</li>
+                    )
+                })
+            }
         </ul>;
     }
 }
